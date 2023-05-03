819c8c3d5e4a1d3e1c1e994c07c1fb39ba25ad0a
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-05-03 07:09:53 -0300

    funcao global para validar inputs

--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -1,9 +1,14 @@
-const dataNascimento = document.querySelector('#nascimento');
+ export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
 
-                            //'blu' é quando input perde o foco.
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDataNascimento(evento.target);
-})
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value);
