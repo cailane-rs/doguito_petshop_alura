c761ebdc74fca7130290db5f9bc7e8b959f20912
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-05-04 06:28:41 -0300

    melhorando exibição de mensagens de erro para cada input

--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -2,12 +2,57 @@
     const tipoDeInput = input.dataset.tipo;
 
     if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
+        validadores[tipoDeInput](input);
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    }
+}
+
+const tipoDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, deve conter pelo menos uma letra maiuscúla, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo da data de nascimento não pode estar vazio.',
+        customError: "Você deve ser maior que 18 anos para se cadastrar."
     }
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = '';
+
+    tipoDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro];
+        }
+    })
+    return mensagem;
+
 }
 
 function validaDataNascimento(input) {
