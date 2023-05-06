e641bc58da71e9f6435ae36bf6e1a0f770721426
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-05-06 14:41:50 -0300

    Campos do endereço preenchidos automaticamente

--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -179,12 +179,25 @@ function recuperarCEP(input) {
             response => response.json()
         ).then(
             data => {
+                console.log(data)
                 if(data.erro) {
                     input.setCustomValidity('Não foi possível buscar o CEP.');
                     return
                 }
                 input.setCustomValidity('')
+                preencheCamposComCEP(data);
+                return
             }
         )
     }
+}
+
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]');
+    const cidade = document.querySelector('[data-tipo="cidade"]');
+    const estado = document.querySelector('[data-tipo="estado"]');
+
+    logradouro.value = data.logradouro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
 }
