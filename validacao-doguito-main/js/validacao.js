a653effc64299717abf7d4780d25a839b9096a1d
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-05-06 14:30:42 -0300

    Trabalhando com API da ViaCep para retornar um CEP válido

--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -38,14 +38,29 @@ const mensagensDeErro = {
         customError: "Você deve ser maior que 18 anos para se cadastrar."
     },
     cpf: {
-        valueMissing: 'O campo de CPF não pode estar vazio',
+        valueMissing: 'O campo de CPF não pode estar vazio.',
         customError: 'O CPF digitado não é válido.'
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP digitado não é válido.',
+        customError: 'Não foi possível buscar o CEP.'
+    },
+    logradouro: {
+        valueMissing: 'O campo de logradouro não pode estar vazio.'
+    },
+    cidade: {
+        valueMissing: 'O campo da cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing: 'O campo do estado não pode estar vazio.'
     }
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input),
-    cpf: input => validaCPF(input)
+    cpf: input => validaCPF(input),
+    cep: input => recuperarCEP(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -138,8 +153,6 @@ function checaDigitoVerificador(cpf, multiplicador) {
     }
 
     if(digitoVerificador == confirmaDigito(soma)) {
-        console.log(digitoVerificador);
-
         return checaDigitoVerificador(cpf, multiplicador + 1);
     }
 
@@ -148,4 +161,30 @@ function checaDigitoVerificador(cpf, multiplicador) {
 
 function confirmaDigito(soma) {
     return 11 - (soma % 11);
+}
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '') //substitui tudo que não for número por string vazia, ou seja, nada
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if(!input.validity.patternMismatch && !input.validity.valueMissing)  {
+        fetch(url, options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.');
+                    return
+                }
+                input.setCustomValidity('')
+            }
+        )
+    }
 }
